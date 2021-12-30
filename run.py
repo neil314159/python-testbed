769f725bcd880f7c4436f616e755fcaf9e331104
neil314159 <neil@neilboland.com>
2021-12-30 19:31:34 +0000

    Test color

--- run.py
+++ run.py
@@ -1,12 +1,41 @@
 import curses
+import random
+import time
 
-screen = curses.initscr()
+g_map = (
+        (1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+        (1, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+        (1, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+        (1, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+        (1, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+        (1, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+        (1, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+        (1, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+        (1, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+        (1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+)
 
-try:
-    screen.border(0)
-    box1 = screen.subwin(20, 20, 5, 5)
-    box1.box()
-    screen.getch()
+g_tileCharsMap = {0:' ', 1:'H'}
 
-finally:
-    curses.endwin()
+def drawMap(_screen, _map):
+    for row in range(len(_map)):
+        for col in range(len(_map[row])):
+            _screen.addch(row, col, g_tileCharsMap[_map[row][col]])
+
+stdscr = curses.initscr()
+stdscr.nodelay(1) #cursor.getch won't wait for input
+curses.curs_set(0) #hide cursor
+
+drawMap(stdscr, g_map)
+
+while(True):
+    newX = random.randint(1, 8)
+    newY = random.randint(1, 8)
+    stdscr.addch(newX, newY, 'x')
+    stdscr.refresh()
+    time.sleep(0.1)
+    stdscr.addch(newX, newY, ' ')
+
+    if(stdscr.getch() >= 0): break #exit on any key pressed
+
+curses.endwin()
