f15d9f59e2e8064d886fba02f17ab1a78a531d3a
neil314159 <neil@neilboland.com>
2021-12-30 17:57:11 +0000

    test TUI layout

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ from google.oauth2.service_account import Credentials
 
 def main():
     print('Hello')
+    stdscr.clear()
+    stdscr.addstr(10, 10, "hello")
 
 print('Welcome to Love Sandiwches automation')
 main()
