1bc35abaa1a162165148b6b5755a87bfade0a80b
neil314159 <neil@neilboland.com>
2021-12-30 18:22:58 +0000

    update

--- run.py
+++ run.py
@@ -10,9 +10,8 @@ def main(stdscr):
     stdscr.clear()
 
     # This raises ZeroDivisionError when i == 10.
-    for i in range(0, 11):
-        v = i-10
-        stdscr.addstr(i, 0, '10 divided by {} is {}'.format(v, 10/v))
+    
+        stdscr.addstr("test")
 
     stdscr.refresh()
     stdscr.getkey()
